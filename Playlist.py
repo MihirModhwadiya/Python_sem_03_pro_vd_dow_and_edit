56149223c106b2f49f4678006fce807598197da6
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2024-03-01 12:03:01 +0530

    multi-download

--- Playlist.py
+++ Playlist.py
@@ -1,16 +1,21 @@
 from pytube import YouTube, Playlist
 from moviepy.editor import *
+from multiprocessing import Pool
 
 def download_video(video_url, download_path):
     yt = YouTube(video_url)
-    yt.streams.filter(progressive=True, file_extension='mp4').order_by(
-        'resolution')[-1].download(download_path)
-    print('Video Downloaded!')
+    yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution")[
+        -1
+    ].download(download_path)
+    print("Video Downloaded:", yt.title)
+
 
 def download_playlist(playlist_url, download_path):
     playlist = Playlist(playlist_url)
-    for video_url in playlist.video_urls:
-        download_video(video_url, download_path)
+    video_urls = playlist.video_urls
+    with Pool(processes=4) as pool:
+        pool.starmap(download_video, [(url, download_path) for url in video_urls])
+
 
 if __name__ == "__main__":
     while True:
@@ -20,11 +25,13 @@ if __name__ == "__main__":
 
         if choice == "1":
             video_url = input("Enter video link: ")
-            download_path = "/yt_v/_lofi/"  # path of folder where you want to download your video
+            download_path = (
+                "/yt_v/_lofi/"  # path of folder where you want to download your video
+            )
             download_video(video_url, download_path)
         elif choice == "2":
             playlist_url = input("Enter playlist link: ")
-            download_path = "/yt_v/_lofi/"  # path of folder where you want to download your videos
+            download_path = "/yt_v/_soft_skills/"  # path of folder where you want to download your videos
             download_playlist(playlist_url, download_path)
         else:
             print("Invalid choice. Please enter 1 or 2.")
